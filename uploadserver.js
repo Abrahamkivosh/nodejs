b121ec7a7242b821b2a4da84c4aed73181476d12
abrahamkivosh <abrahamkivosh@gmail.com>
2022-08-27 12:30:31 +0300

    able to upload file

--- uploadserver.js
+++ uploadserver.js
@@ -10,12 +10,23 @@ const server = http.createServer((req, res)=>{
             console.log(files)
             res.end("check");
             var oldpath = files.filetoupload.filepath;
-            var newpath = '/home/kivosh/Documents/projects/learn/js/nodejs/one/' + files.filetoupload.originalFilename;
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) throw err;
-                res.write('File uploaded and moved!');
-                res.end();
-              });
+            var newpath = '/home/kivosh/Documents/projects/learn/js/nodejs/' + files.filetoupload.originalFilename;
+            
+            fs.readFile(oldpath,(err, data)=>{
+                if(err) throw err ;
+                // write the file to newpath
+                fs.writeFile(newpath,data,(err)=>{
+                    if(err) throw err ;
+                    res.end()
+                    console.log("file moved")
+                })
+
+                // unlink from temp
+                fs.unlink(oldpath,err=>{
+                    if(err) throw err ;
+                    console.log("file deleted from temp")
+                })
+            })
         })
         
     }else{
